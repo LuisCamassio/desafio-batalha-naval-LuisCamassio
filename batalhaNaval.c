bf5407583b8dc39a8414f92ed975f887bf047975
LuisCamassio <61327186+LuisCamassio@users.noreply.github.com>
2025-09-06 16:02:31 +0000

    intermediario

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,42 +1,53 @@
 #include <stdio.h>
 
 int main() {
-    // Dimensão fixa do tabuleiro
-    const int tamanho = 10;
-    int tabuleiro[10][10];
+    // Tabuleiro 10x10 inicializado com 0 (representa água)
+    int tabuleiro[10][10] = {0};
+    int i, j;
 
-    // Inicializa todo o tabuleiro com 0 (água)
-    for (int i = 0; i < tamanho; i++) {
-        for (int j = 0; j < tamanho; j++) {
-            tabuleiro[i][j] = 0;
+    // Tamanho fixo dos navios
+    int tamanhoNavio = 3;
+
+    // ---------------------------
+    // Posicionando os navios retos
+    // ---------------------------
+
+    // Navio 1 - horizontal (linha 0, colunas 0 a 2)
+    for (i = 0; i < tamanhoNavio; i++) {
+        tabuleiro[0][i] = 3;
+    }
+
+    // Navio 2 - vertical (coluna 5, linhas 2 a 4)
+    for (i = 0; i < tamanhoNavio; i++) {
+        tabuleiro[2 + i][5] = 3;
+    }
+
+    // ---------------------------
+    // Posicionando os navios diagonais
+    // ---------------------------
+
+    // Navio 3 - diagonal principal (começa em [6][6])
+    for (i = 0; i < tamanhoNavio; i++) {
+        tabuleiro[6 + i][6 + i] = 3;
+    }
+
+    // Navio 4 - diagonal secundária (começa em [4][6])
+    for (i = 0; i < tamanhoNavio; i++) {
+        tabuleiro[4 + i][6 - i] = 3;
+    }
+
+    // ---------------------------
+    // Exibindo o tabuleiro
+    // ---------------------------
+    printf("Tabuleiro Batalha Naval (10x10):\n\n");
+
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]); // imprime linha
         }
-    }
-
-    // Tamanho dos navios
-    const int tamNavio = 3;
-
-    // Coordenadas iniciais dos navios (fixas neste nível)
-    int linhaNavioH = 2, colunaNavioH = 1; // Navio horizontal começa em (2,1)
-    int linhaNavioV = 5, colunaNavioV = 4; // Navio vertical começa em (5,4)
-
-    // Posiciona navio horizontal (linha fixa, varia a coluna)
-    for (int i = 0; i < tamNavio; i++) {
-        tabuleiro[linhaNavioH][colunaNavioH + i] = 3;
-    }
-
-    // Posiciona navio vertical (coluna fixa, varia a linha)
-    for (int i = 0; i < tamNavio; i++) {
-        tabuleiro[linhaNavioV + i][colunaNavioV] = 3;
-    }
-
-    // Exibe o tabuleiro
-    printf("=== Tabuleiro Batalha Naval ===\n\n");
-    for (int i = 0; i < tamanho; i++) {
-        for (int j = 0; j < tamanho; j++) {
-            printf("%d ", tabuleiro[i][j]);
-        }
-        printf("\n");
+        printf("\n"); // quebra de linha
     }
 
     return 0;
 }
+
